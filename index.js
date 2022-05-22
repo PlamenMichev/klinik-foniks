92416aa8787aa17c7a8880b30da80edf609b713a
Plamen Michev <plamenmichev15@gmail.com>
2022-05-22 16:30:07 +0200

    add explanation page

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 document.addEventListener('scroll', function (e) {
     var top  = window.pageYOffset + window.innerHeight,
-        isVisible = top > document.getElementById('flower-animation').offsetTop;
-  
+        isVisible = top > document.getElementById('flower-animation')?.offsetTop;
+        isVisiblePage = top > document.getElementById('flower-animation-second')?.offsetTop;
+    console.log(isVisiblePage)
+        if(isVisiblePage) {
+          document.getElementById('flower-animation-second').classList.add('animate-rotate');
+        }
+
      if (isVisible) {
        document.getElementById('flower-animation').classList.add('animate-rotate');
      }
